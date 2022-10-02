fcf9dc0ad038b38a1029243156e7407da9f59d8e
ReenaH <Reena_Hunjan@yahoo.com>
2022-10-01 21:32:35 -0400

    add jwt

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+    friends: [User]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    helloWorld: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
